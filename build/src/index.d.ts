ebf355a9f966456a380cdcb91cba7b83c71147fb
Orbit Turner <orbitturner@gmail.com>
2022-02-04 11:55:56 +0000

    feat(ORBITCHAIN): :zap: CLEANING

--- build/src/index.d.ts
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env node
-declare const vorpal: any;
--- build/src/index.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-const vorpal = require('vorpal')();
-vorpal.use(require('./cli.js'));
-//# sourceMappingURL=index.js.map
